fa11d141366163d4aa9817daac0e55def916a5dd
annu <annuverma8234@gmail.com>
2024-07-24 08:46:26 +0530

    clock

--- clc.js
+++ clc.js
@@ -1,5 +1,3 @@
-// let date = new Date('March 16,2024');
-// console.log( date.getDay());
 
 let display = document.getElementById('clock');
 function showTime() {
@@ -9,7 +7,7 @@ function showTime() {
     let s = d.getSeconds();
     let session = "AM"
 
-    if (h <12) {
+    if (h > 12) {
         h = h - 12;
     }
     if (h>= 12) {
@@ -27,31 +25,6 @@ showTime();
 
 
 
-
-
-// let display = document.getElementById('clock')
-
-// function showTime() {
-//     let d = new Date();
-//     let h = d.getHours();
-//     let m = d.getMinutes();
-//     let s = d.getSeconds();
-//     let session = "AM";
-//     if (h > 12) {
-//         h = h - 12;
-//     }
-//     if (h>= 12) {
-//         session = "PM";
-//     }
-
-//     h = (h < 10) ? "0" + h : h;
-//     m = (m < 10) ? "0" + m : m;
-//     s = (s < 10) ? "0" + s : m;
-//     let time = h + ":" + m + ":" + s + " " + session;
-//     document.getElementById("clock").innerText = time;
-//     setTimeout(showTime, 1000);
-// }
-// showTime();
 
 
 
